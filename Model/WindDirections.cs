946b99abb7c70d60cd7661409e709ebd8bf35be2
partoffotrap <a.borbotko.sas@gmail.com>
2023-05-18 17:33:47 +0500

    refactor wind direction calculator class

--- Model/WindDirections.cs
+++ Model/WindDirections.cs
@@ -4,15 +4,15 @@
     {
         public static string GetDirectionNameByDegree(int degree)
         {
-            if (degree >= 0 && degree <= 23 || degree >= 337 && degree <= 360) return "С ↑";
-            else if (degree >= 24 && degree <= 68) return "СВ ↗";
-            else if (degree >= 69 && degree <= 113) return "В →";
-            else if (degree >= 114 && degree <= 158) return "ЮВ ↘";
-            else if (degree >= 159 && degree <= 203) return "Ю ↓";
-            else if (degree >= 204 && degree <= 248) return "ЮЗ ↙";
-            else if (degree >= 249 && degree <= 293) return "З ←";
-            else if (degree >= 294 && degree <= 336) return "СЗ ↖";
-            else return "неопределено";
+            if (degree >= 337) return "С ↑";
+            if (degree >= 294) return "СЗ ↖";
+            if (degree >= 249) return "З ←";
+            if (degree >= 204) return "ЮЗ ↙";
+            if (degree >= 159) return "Ю ↓";
+            if (degree >= 114) return "ЮВ ↘";
+            if (degree >= 69) return "В →";
+            if (degree >= 24) return "СВ ↗";
+            return "С ↑";
         }
     }
 }
